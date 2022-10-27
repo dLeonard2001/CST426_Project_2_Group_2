655d86a3e504b24e6112dc40d91b764da2d47ef6
asylum14 <michaelanthonysilva18@gmail.com>
2022-10-26 22:59:43 -0700

    Added More detailed data flow
    I added a more robust data flow to allow data to come from an object to a save file.

--- roguelike_crafter/Assets/Scripts/dataManager.cs
+++ roguelike_crafter/Assets/Scripts/dataManager.cs
@@ -3,12 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 public class dataManager : MonoBehaviour
 {
+    private GameObject test;
     public int coins;
+    public int level;
+    public int atk;
+    public float crit_chance;
+    public float crit_multiplyer;
+    public int num_gears;
     // Start is called before the first frame update
     void Start()
-    {
+    {   
+        
+        //loadData();
         saveData();
-        loadData();
+        
     }
 
 
@@ -16,14 +24,34 @@ public class dataManager : MonoBehaviour
     void loadData()
     {
         string data = System.IO.File.ReadAllText(Application.persistentDataPath + "/Data.json");
-        PlayerData pd2 = PlayerData.CreateFromJSON(data);
-        Debug.Log(pd2);
+        PlayerData pd = PlayerData.CreateFromJSON(data);
         Debug.Log("Load Successful");
+        coins = pd.coins;
+        level = pd.level;
+        atk = pd.atk;
+        crit_chance = pd.crit_chance;
+        crit_multiplyer = pd.crit_multiplyer;
+        num_gears = pd.num_gears;
+
+        Debug.Log("Coins: " + coins +
+                  "\nLevel: " + level +
+                  "\nAttack Power: " + atk +
+                  "\nCritical Hit Chance: " + crit_chance
+                  + "\nCritical Hit Multiplyer: " + crit_multiplyer +
+                  "\nNumber of Gears: " + num_gears);
     }
 
     void saveData()
     {
-        coins = 7;
+        //get data from player object
+        PlayerData tpd = TestPlayerManager.getPlayerData();
+        print(tpd);
+        coins = tpd.coins; 
+        level = tpd.level;
+        atk = tpd.atk;
+        crit_chance = tpd.crit_chance;
+        crit_multiplyer = tpd.crit_multiplyer;
+        num_gears = tpd.num_gears;
         string data = JsonUtility.ToJson(this);
 
         System.IO.File.WriteAllText(Application.persistentDataPath + "/Data.json", data);
